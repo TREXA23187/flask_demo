d8c7668664ee85592efa42877ba18ef7f8156232
TREXA23187 <498111557@qq.com>
2023-08-01 16:24:59 +0100

    feat: send model trained file

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 from model.enter import run
 import json
 import requests
-import sys
+import base64
 
 app = Flask(__name__)
 
-localhost = "host.docker.internal"
+# localhost = "host.docker.internal"
+localhost = "localhost"
 # localhost = "47.243.60.114"
 
 with open('config.json', 'r', encoding='utf-8') as fp:
@@ -17,8 +18,11 @@ try:
 
     with open('./model/model.pickle', 'rb') as f:
         model_file = f.read()
+
+    model_file_base64 = base64.b64encode(model_file).decode()
     response = requests.post(f'http://{localhost}:8080/api/v1/console/task/operate',
-                             json={"task_id": config["taskId"], "operation": "success", "model_file": model_file})
+                             json={"task_id": config["taskId"], "operation": "success",
+                                   "model_file": model_file_base64})
 except Exception as e:
     print(e)
     response = requests.post(f'http://{localhost}:8080/api/v1/console/task/operate',
@@ -31,4 +35,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
